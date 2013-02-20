45d34500d12d78b9e09907ed40712005926a6425
vinceMeens <v.w.a.meens@comcordis.com>
2013-02-20 19:46:49 +0100

    Fixed loading

--- initialize.php
+++ initialize.php
@@ -4,6 +4,4 @@ require_once 'XXX_FileSystem_Local.php';
 require_once 'XXX_Log.php';
 
 XXX::addEventListener('beforeExecutionExit', 'XXX_Log::saveBuffers');
-
-echo 'Filesystem local is loaded';
 ?>
